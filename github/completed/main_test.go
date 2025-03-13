c4718389576da48597005a9b55fe888f5ea707b9
Adrian Riobo <ariobolo@redhat.com>
2025-03-13 08:14:15 +0100

    chore: estructura inicial para sesion github
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

---
+++ github/completed/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHelloHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "/hello", nil)
+	w := httptest.NewRecorder()
+
+	helloHandler(w, req)
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("esperado status %d, obtenido %d", http.StatusOK, resp.StatusCode)
+	}
+
+	expectedBody := "Hello, world!"
+	if w.Body.String() != expectedBody {
+		t.Errorf("esperado cuerpo %q, obtenido %q", expectedBody, w.Body.String())
+	}
+}
